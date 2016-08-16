3af416f97f96c572b0d1d6e47243f064a1fee94e
yuki-ike <yuki.i121010@gmail.com>
2016-08-17 01:29:40 +0900

    add resources :prototypes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'pages#index'
-  get 'pages/show'
+  root 'prototypes#index'
+  resources :prototypes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
