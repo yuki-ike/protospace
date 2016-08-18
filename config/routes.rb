b69f274a67d12792559589f9c24881e8ebf3ac14
yuki-ike <yuki.i121010@gmail.com>
2016-08-19 00:25:52 +0900

    add edit_user function at routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   resources :prototypes
+  resources :users do
+    get 'edit'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
