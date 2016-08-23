aa66ff08568388f1f9c5e5f754a60d159807dcfb
yuki-ike <yuki.i121010@gmail.com>
2016-08-24 00:39:07 +0900

    rails g controller users

--- db/migrate/20160816051315_devise_create_users.rb
+++ db/migrate/20160816051315_devise_create_users.rb
@@ -28,10 +28,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
+      t.datetime :locked_at
 
 
-      t.timestamps null: false
+      t.timestamps
     end
 
     add_index :users, :email,                unique: true
