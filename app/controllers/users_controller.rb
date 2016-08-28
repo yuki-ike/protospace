f4f2c30f396a981ed686079a36a186f56697693c
yuki-ike <yuki.i121010@gmail.com>
2016-08-29 00:44:57 +0900

    add update_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,16 @@ class UsersController < ApplicationController
 	end
 
 	def update
+		current_user.update(update_params)
+		redirect_to "/"
 	end
 
 	def show
 		@user = User.find(params[:id])
+	end
+
+	private
+	def update_params
+		params.require(:user).permit(:username, :member_of, :profile, :works)
 	end
 end
