6f0c1bc843c2d6fe9a3e86ceab36d16455c45fcc
yuki-ike <yuki.i121010@gmail.com>
2016-08-29 17:01:59 +0900

    edit indent

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
 
-def edit
-end
+  def edit
+  end
 
-def update
-	current_user.update(update_params)
-	redirect_to "/"
-end
+  def update
+    current_user.update(update_params)
+    redirect_to "/"
+  end
 
-def show
-	@user = User.find(params[:id])
-end
+  def show
+    @user = User.find(params[:id])
+  end
 
-private
-def update_params
-	params.require(:user).permit(:username, :member_of, :profile, :works)
-end
+  private
+  def update_params
+    params.require(:user).permit(:username, :member_of, :profile, :works)
+  end
 
 end
