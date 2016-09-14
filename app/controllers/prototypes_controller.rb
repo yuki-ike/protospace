427799122a7455889a14d8d9e76e9e215a2e9671
yuki-ike <yuki.i121010@gmail.com>
2016-09-14 23:44:56 +0900

    prototype controller and index_view

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,28 @@
 class PrototypesController < ApplicationController
   def index
+    @prototypes = Prototype.includes(:user).all
+  end
+
+  def new
+    @prototype = Prototype.new
+    @main_image = @prototype.contents.build
+    @sub_images = @prototype.contents.build
+  end
+
+  def create
+    Prototype.create(prototype_params)
+    redirect_to "/"
+  end
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(
+      :title,
+      :catch_copy,
+      :content,
+      contents_attributes:[:id, :content, :status]).merge(user_id: current_user.id)
   end
 end
