9017bfab653bdb3f762dd9f51fd1171f20072cd4
yuki-ike <yuki.i121010@gmail.com>
2016-09-27 19:49:39 +0900

    add models and routes for likes_table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
-  resources :prototypes
+  resources :prototypes do
+    resources :likes, only: [:create, :destroy]
+  end
   resources :users, only: [:edit, :show, :update]
   end
