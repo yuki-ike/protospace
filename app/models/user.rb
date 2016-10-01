039745c2bebef9f42fa15e3a0084d908a7b3d26d
yuki-ike <yuki.i121010@gmail.com>
2016-10-01 19:30:16 +0900

    add dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :prototypes
-  has_many :likes
-  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
 end
