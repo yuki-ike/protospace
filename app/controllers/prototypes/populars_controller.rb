0679db313da416c24d2e8e8d7e13e9707c1d6372
yuki-ike <yuki.i121010@gmail.com>
2016-10-23 22:09:10 +0900

    edit => and N+1

--- app/controllers/prototypes/populars_controller.rb
+++ app/controllers/prototypes/populars_controller.rb
@@ -1,5 +1,5 @@
 class Prototypes::PopularsController < ApplicationController
   def index
-    @prototypes = Prototype.eager_load(:user).includes(:tags).order('likes_count DESC')
+    @prototypes = Prototype.eager_load(:user, :tags).order('likes_count DESC')
   end
 end
