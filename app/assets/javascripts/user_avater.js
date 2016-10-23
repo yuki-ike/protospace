319432c4f45b8ed0308153bba5f146f8a109f038
yuki-ike <yuki.i121010@gmail.com>
2016-10-23 10:22:03 +0900

    add user_avater_image and edit user_avater_image

---
+++ app/assets/javascripts/user_avater.js
@@ -0,0 +1,37 @@
+$(window).on("load", function(){
+    var setFileInput = $('.upload_avater');
+
+    setFileInput.each(function(){
+        var selfFile = $(this),
+        selfInput = $(this).find('input[type=file]');
+
+        selfInput.change(function(){
+            var file = $(this).prop('files')[0],
+            fileRdr = new FileReader(),
+            selfImg = selfFile.find('.avater_View');
+
+            if(!this.files.length){
+                if(0 < selfImg.size()){
+                    selfImg.remove();
+                    return;
+                }
+            } else {
+                if(file.type.match('image.*')){
+                    if(!(0 < selfImg.size())){
+                        selfFile.append('<img alt="" class="avater_View">');
+                    }
+                    var prevElm = selfFile.find('.avater_View');
+                    fileRdr.onload = function() {
+                        prevElm.attr('src', fileRdr.result);
+                    }
+                    fileRdr.readAsDataURL(file);
+                } else {
+                    if(0 < selfImg.size()){
+                        selfImg.remove();
+                        return;
+                    }
+                }
+            }
+        });
+    });
+});
