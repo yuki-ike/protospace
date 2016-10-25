1ef9ce7e7b43ca1938d12bb65971717b509e8314
yuki-ike <yuki.i121010@gmail.com>
2016-10-25 17:53:33 +0900

    edit prototypes controller and view

--- app/controllers/prototypes/newests_controller.rb
+++ app/controllers/prototypes/newests_controller.rb
@@ -1,5 +1,6 @@
 class Prototypes::NewestsController < ApplicationController
   def index
-    @prototypes = Prototype.includes(:user, :tags).order("created_at DESC")
+    @prototypes = Prototype.includes(:user, :tags).order("created_at DESC").page(params[:page]).per(8)
+  end
   end
 end
