2bef28d2a1bf6d0eaaea7b8030800e8a79864cb1
yuki-ike <yuki.i121010@gmail.com>
2016-10-25 17:57:20 +0900

    delete 'end' for syntax error

--- app/controllers/prototypes/newests_controller.rb
+++ app/controllers/prototypes/newests_controller.rb
@@ -2,5 +2,4 @@ class Prototypes::NewestsController < ApplicationController
   def index
     @prototypes = Prototype.includes(:user, :tags).order("created_at DESC").page(params[:page]).per(8)
   end
-  end
 end
